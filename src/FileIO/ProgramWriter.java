94e0d0bcf85a56b256acadb9615866349a4db5c4
Adam <acf5118@rit.edu>
2016-01-10 21:27:11 -0500

    removed accidental class files

--- src/FileIO/ProgramWriter.java
+++ src/FileIO/ProgramWriter.java
@@ -1,7 +1,43 @@
 package FileIO;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created by Adam Fowles on 1/10/2016.
  */
-public class ProgramWriter {
+public class ProgramWriter
+{
+    private double[] params;
+
+    public ProgramWriter(double[] params)
+    {
+        this.params = params;
+    }
+
+    public void writeFile(String filename)
+    {
+        BufferedWriter writer = null;
+        try {
+            File f = new File(filename);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(filename);
+            writer = new BufferedWriter(fileWriter);
+
+
+            writer.write("c Program file\n");
+            writer.write("c <length> <x1> <x2> <x3> <y1> <y2> <y3> <f1> <f2>\n");
+            writer.write("p ");
+            for (double d : params) {
+                writer.write(d + " ");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Couldnt write file");
+        }
+    }
 }
--- src/GCodeUtil/GCodeGenerator.java
+++ src/GCodeUtil/GCodeGenerator.java
@@ -1,7 +1,34 @@
 package GCodeUtil;
 
+import java.util.ArrayList;
+
 /**
  * Created by Adam Fowles on 1/10/2016.
  */
-public class GCodeGenerator {
+public class GCodeGenerator
+{
+    public GCodeGenerator()
+    {
+
+    }
+
+    public ArrayList<String> generateLines(double[] params)
+    {
+        ArrayList<String> lines = new ArrayList<>();
+
+        lines.add("G20\n");
+        lines.add("G91 G00 Y" + params[4] + "\n");
+        // Needs to pause GCode
+        lines.add("G91 G01 X" + params[1] + " Y" + params[5] + " F" + params[8] + "\n");
+        lines.add("G91 G01 X" + params[2] + " F" + params[7] + "\n");
+        lines.add("G91 G01 X" + params[3] + " Y" + params[6] + " F" + params[8] + "\n");
+        lines.add("G91 G01 X" + params[2] + " F" + params[7] + "\n");
+        lines.add("G91 G01 X" + params[1] + " Y" + params[5] + " F" + params[8] + "\n");
+        for (String s: lines)
+        {
+            System.out.println(s);
+        }
+        return lines;
+    }
+
 }
